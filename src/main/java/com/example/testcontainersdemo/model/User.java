b1a97db216991ea526bfbf2ad77433d634fca36e
romanthium <romanthium@gmail.com>
2023-12-04 16:26:35 +0100

    added builder to User class

--- src/main/java/com/example/testcontainersdemo/model/User.java
+++ src/main/java/com/example/testcontainersdemo/model/User.java
@@ -1,14 +1,12 @@
 package com.example.testcontainersdemo.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name="user_data")
