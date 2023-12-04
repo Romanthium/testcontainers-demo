048d3d0605c978e35a70dace93d4cf0524e7c0dd
romanthium <romanthium@gmail.com>
2023-12-04 15:05:09 +0100

    added User entity

---
+++ src/main/java/com/example/testcontainersdemo/model/User.java
@@ -0,0 +1,26 @@
+package com.example.testcontainersdemo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="user_data")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    // Getters and Setters
+}
